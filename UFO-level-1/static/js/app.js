33bd50cef38d28b6a3343423783c7ca01f19f877
dfirestone73 <52640393+dfirestone73@users.noreply.github.com>
2019-10-26 12:17:11 -0500

    pushing

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,23 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var table = d3.select("#ufo-table");
-var button = d3.select("#filter-brn");
-
-
+var button = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
 
 button.on("click", function () {
+    tbody.html("");
     var input = d3.select("#datetime");
     var inputValue = input.property("value");
 
-    var filteredData = tableData.filter(dates => dates.datetime === inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
-    tableData.forEach(function (UFOReport) {
-        var row = table.append("tr");
+    tbody = d3.select("tbody");
+
+    filteredData.forEach(function (UFOReport) {
+        var row = tbody.append("tr");
         Object.entries(UFOReport).forEach(function ([key, value]) {
+            console.log(key,value);
             var cell = row.append("td");
             cell.text(value);
         });
-    })
+    });
 
 });
